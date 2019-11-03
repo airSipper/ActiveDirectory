1fcefa3a433f4c97dc816e438d30995942effe39
airSipper <luke543@gmail.com>
2019-11-03 18:07:27 +1100

    Update getconfigs.py

--- getconfigs.py
+++ getconfigs.py
@@ -1,52 +1,57 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # this script connects to all our networking devices and 
-# writes their running-config to file [[device ip].conf]
+# writes their running-config to file [[date]_[device ip].txt]
+#
+# Credentials will be checked against either the local device database OR the TACACS servers
 
 from netmiko import Netmiko
-#from getpass import getpass
+from datetime import date
+from getpass import getpass
 
+username = str(input('Username: '))
+password = getpass()
 
 SYDGW = {
     "host": "172.16.5.254",
-    "username": 'admin',
-    "password": 'Password01',
+    "username": username,
+    "password": password,
     "device_type": "cisco_ios",
-	"secret": "Password01"
+	"secret": password
 }
 
 SYDSW1 = {
     "host": "172.16.5.253",
-    "username": 'admin',
-    "password": 'Password01',
+    "username": username,
+    "password": password,
     "device_type": "cisco_ios",
-	"secret": "Password01"
+	"secret": password
 }
 
 SYDSW2 = {
     "host": "172.16.5.252",
-    "username": 'admin',
-    "password": 'Password01',
+    "username": username,
+    "password": password,
     "device_type": "cisco_ios",
-	"secret": "Password01"
+	"secret": password
 
 }
 
 CBRGW = {
     "host": "172.17.2.254",
-    "username": 'admin',
-    "password": 'Password01',
+    "username": username,
+    "password": password,
     "device_type": "cisco_ios",
-	"secret": "Password01"
+	"secret": password
 
 }
 
 CBRSW1 = {
     "host": "172.17.2.253",
-    "username": 'admin',
-    "password": 'Password01',
+    "username": username,
+    "password": password,
     "device_type": "cisco_ios",
-	"secret": "Password01"
+	"secret": password
 }
 
 for device in (SYDGW, SYDSW1, SYDSW2,CBRGW, CBRSW1):
@@ -60,6 +65,7 @@ for device in (SYDGW, SYDSW1, SYDSW2,CBRGW, CBRSW1):
 	print(output)
 	print()
 
-	f = open(device["host"] + '.conf', 'w')
+	f = open(str(date.today() + "_" + (device["host"] + '.txt')), 'w')
 
 	f.write(output)
+	f.close()
